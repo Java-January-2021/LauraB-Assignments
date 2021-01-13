79d0c9bd2a750cb900db15a9689cfeabe81103a4
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-01-13 18:02:15 -0500

    Java Fundamentals, First Java Program

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,27 @@
+public class FirstJavaProgram {
+    //entry point method
+    public static void main(String[] args) { 
+        System.out.println("My name is Laura Bullock.");
+        System.out.println("I am, happy to be alive, years old.");
+        System.out.println("My hometown is Northern Virginia, USA.");
+    }
+}
+
+//In this assignment, you will create your own Java Program that will print your name, age, and hometown. Once you run your bytecode, you should see the following output:
+
+// My name is Coding Dojo
+// I am 100 years old
+// My hometown is Burbank, CA
+// Objectives:
+// - Learn about the java program life cycle.
+
+// - Start coding java right away.
+
+// Tasks:
+// - Create a new source file inside of your javaFun directory.
+
+// - In the main method, implement the code to print your name, age, and hometown in different lines.
+
+// - Compile your code without errors.
+
+// - Run your .class file.
